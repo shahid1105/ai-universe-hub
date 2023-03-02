2f0bed84c0695f29a961dd5fd83834fad65abd57
shahid1105 <shahidhasan1105@gmail.com>
2023-03-02 23:04:17 +0600

    set card all information

--- js/index.js
+++ js/index.js
@@ -10,15 +10,28 @@ const showAllData = (data) => {
     const cardContainer = document.getElementById('card');
     
     data.forEach(singleData => {
-        const cardDiv = document.createElement('div');
-    cardDiv.classList.add('col');
+        console.log(singleData);
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('col',);
     cardDiv.innerHTML = `
-    <div class="card h-100">
-    <img src="..." class="card-img-top" alt="...">
+    <div class="card h-100 p-3">
+    <img style="height: 300px; width: 437;" class="img-fluid rounded-2" src="${singleData.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
+      <h5 class="card-title mb-4 fw-bold">Features</h5>
+      <p class="card-text">1. ${singleData.features[0]}</p>
+      <p class="card-text">2. ${singleData.features[1]}</p>
+      <p class="card-text">3. ${singleData.features[2]}</p>
+      </div>
+      <hr>
+      <div class="d-flex flex-row justify-content-between align-items-center">
+            <div>
+                <h5 class="card-title mb-4 fw-bold">${singleData.name}</h5>
+                <p class="card-text"><i class="fa-solid fa-calendar-days"></i> ${singleData.published_in}</p>
+            </div>
+            <div>
+                <p><i class="fa-solid fa-arrow-right text-danger border bg-danger-subtle rounded-circle p-2"></i></p>
+            </div>
+      </div>
   </div>
     `
     cardContainer.appendChild(cardDiv)
