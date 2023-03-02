c9ba724e95a69ef5f222d4330db147cdef0ea3d1
shahid1105 <shahidhasan1105@gmail.com>
2023-03-02 20:55:33 +0600

    add card and load data in js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('set js file');
+const loadAllData = () =>{
+    const url = ` https://openapi.programming-hero.com/api/ai/tools`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showAllData(data.data.tools))
+}
+
+const showAllData = (data) => {
+    console.log(data);
+}
+
+loadAllData();
